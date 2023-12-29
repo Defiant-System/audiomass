6bb8c93de028c1547202bd896f69cb1cc9e98bc6
Hakan Bilgin <hbi99@hotmail.com>
2023-12-29 19:50:00 +0100

    Update audio-utils.js

--- src/js/modules/audio-utils.js
+++ src/js/modules/audio-utils.js
@@ -1,19 +1,33 @@
 
 let AudioUtils = {
 	TrimTo(val, num) {
-		let nums = { '0': 1, '1': 10, '2': 100,'3': 1000,'4': 10000,'5': 100000 };
+		let nums = { "0": 1, "1": 10, "2": 100,"3": 1000,"4": 10000,"5": 100000 };
 		let dec = nums[num];
 		return ((val * dec) >> 0) / dec;
 	},
 
 	LoadDecoded(file, buffer) {
-		let blob = new Blob([buffer], { type: file._file.blob.type });
-		console.log( blob );
-		// file._ws.loadBlob(blob);
+		let [left, right] = [buffer.getChannelData(0), buffer.getChannelData(1)];
+		let interleaved = new Float32Array(left.length + right.length);
+
+		for (let src=0, dst=0; src < left.length; src++, dst+=2) {
+			interleaved[dst] = left[src];
+			interleaved[dst+1] = right[src];
+		}
+
+		// get WAV file bytes and audio params of your audio source
+		let wavBytes = this.getWavBytes(interleaved.buffer, {
+			isFloat: true,
+			numChannels: 2,
+			sampleRate: buffer.sampleRate,
+		});
+
+		let blob = new Blob([wavBytes], { type: "audio/wav" });
+		file._ws.loadBlob(blob);
 	},
 
-	CreateBuffer(channels, length, rate) {
-		return new AudioContext().createBuffer(channels, length, rate);
+	CreateBuffer(channels, length, sampleRate) {
+		return new AudioContext().createBuffer(channels, length, sampleRate);
 	},
 
 	CopyBufferSegment(file) {
@@ -28,8 +42,8 @@ let AudioUtils = {
 
 		let channels = original_buffer.numberOfChannels;
 		let length = duration * original_buffer.sampleRate;
-		let rate = original_buffer.sampleRate;
-		let empty_segment = this.CreateBuffer(channels, length, rate);
+		let sampleRate = original_buffer.sampleRate;
+		let empty_segment = this.CreateBuffer(channels, length, sampleRate);
 
 		for (let i=0, u=0; i<peaks.length; ++i) {
 			// if (wavesurfer.ActiveChannels[ i ] === 0) continue;
@@ -44,12 +58,12 @@ let AudioUtils = {
 	InsertSegmentToBuffer(data) {
 		let originalBuffer = data.file._ws.getDecodedData();
 		let channels = originalBuffer.numberOfChannels;
-		let length = originalBuffer.length * data.buffer.length;
-		let rate = originalBuffer.sampleRate;
-		let uberSegment = this.CreateBuffer(channels, length, rate);
-
+		let length = originalBuffer.length + data.buffer.length;
+		let sampleRate = originalBuffer.sampleRate;
+		let uberSegment = this.CreateBuffer(channels, length, sampleRate);
+		
 		let offset = this.TrimTo(data.file._ws.getCurrentTime(), 3);
-		offset = ((offset / 1) * rate) >> 0;
+		offset = ((offset / 1) * sampleRate) >> 0;
 		
 		for (let i=0; i<channels; ++i) {
 			let chan_data = originalBuffer.getChannelData(i);
@@ -60,19 +74,18 @@ let AudioUtils = {
 								? data.buffer.getChannelData(0)
 								: data.buffer.getChannelData(i);
 
-
 			// check to see if we have only 1 channel selected
 			if (channels === 1) {
 				// check if we have the selected channel
-				if (wavesurfer.ActiveChannels[ i ] === 0) {
+				if (wavesurfer.ActiveChannels[i] === 0) {
 					// keep original
-					uberChanData.set( chan_data );
+					uberChanData.set(chan_data);
 					continue;
 				}
 			}
 
 			if (offset > 0) {
-				uberChanData.set(chan_data.slice (0, offset));
+				uberChanData.set(chan_data.slice(0, offset));
 			}
 
 			uberChanData.set(segment_chan_data, offset);
@@ -85,9 +98,70 @@ let AudioUtils = {
 		}
 		
 		this.LoadDecoded(data.file, uberSegment);
+		
+		// let start = offset / sampleRate,
+		// 	end = start + (data.buffer.length / sampleRate);
+		// return [start, end];
+	},
 
-		let start = offset / rate,
-			end = start + (data.buffer.length / rate);
-		return [start, end];
+	// Returns Uint8Array of WAV bytes
+	getWavBytes(buffer, options) {
+		let type = options.isFloat ? Float32Array : Uint16Array;
+		let numFrames = buffer.byteLength / type.BYTES_PER_ELEMENT;
+		let headerBytes = this.getWavHeader(Object.assign({}, options, { numFrames }));
+		let wavBytes = new Uint8Array(headerBytes.length + buffer.byteLength);
+		// prepend header, then add pcmBytes
+		wavBytes.set(headerBytes, 0);
+		wavBytes.set(new Uint8Array(buffer), headerBytes.length);
+		return wavBytes;
+	},
+
+	// adapted from https://gist.github.com/also/900023
+	// returns Uint8Array of WAV header bytes
+	getWavHeader(options) {
+		let numFrames      = options.numFrames;
+		let numChannels    = options.numChannels || 2;
+		let sampleRate     = options.sampleRate || 44100;
+		let bytesPerSample = options.isFloat? 4 : 2;
+		let format         = options.isFloat? 3 : 1;
+		let blockAlign     = numChannels * bytesPerSample;
+		let byteRate       = sampleRate * blockAlign;
+		let dataSize       = numFrames * blockAlign;
+		let buffer         = new ArrayBuffer(44);
+		let dataView       = new DataView(buffer);
+		let p = 0;
+
+		let writeString = s => {
+			for (let i = 0; i < s.length; i++) {
+				dataView.setUint8(p + i, s.charCodeAt(i));
+			}
+			p += s.length;
+		};
+
+		let writeUint32 = d => {
+			dataView.setUint32(p, d, true);
+			p += 4;
+		};
+
+		let writeUint16 = d => {
+			dataView.setUint16(p, d, true);
+			p += 2;
+		};
+
+		writeString("RIFF");              // ChunkID
+		writeUint32(dataSize + 36);       // ChunkSize
+		writeString("WAVE");              // Format
+		writeString("fmt ");              // Subchunk1ID
+		writeUint32(16);                  // Subchunk1Size
+		writeUint16(format);              // AudioFormat https://i.stack.imgur.com/BuSmb.png
+		writeUint16(numChannels);         // NumChannels
+		writeUint32(sampleRate);          // SampleRate
+		writeUint32(byteRate);            // ByteRate
+		writeUint16(blockAlign);          // BlockAlign
+		writeUint16(bytesPerSample * 8);  // BitsPerSample
+		writeString("data");              // Subchunk2ID
+		writeUint32(dataSize);            // Subchunk2Size
+
+		return new Uint8Array(buffer);
 	}
 };

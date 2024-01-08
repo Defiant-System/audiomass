814460bbb9ff182bf03a81da318c4a2f98ec819f
Hakan Bilgin <hbi99@hotmail.com>
2024-01-08 18:49:47 +0100

    Update audio-fx.js

--- src/js/modules/audio-fx.js
+++ src/js/modules/audio-fx.js
@@ -24,22 +24,31 @@ let AudioFX = {
 		data.offlineCtx.startRendering();
 
 		data.offlineCtx.oncomplete = event => {
+			let region = data.file._activeRegion;
+			let start = region ? region.start : 0;
+			let end = region ? region.end : originalBuffer.duration;
+
 			let renderedBuffer = event.renderedBuffer;
 			let originalBuffer = data.file._ws.getDecodedData();
 			let channels = originalBuffer.numberOfChannels;
 			let sampleRate = originalBuffer.sampleRate;
 			let length = originalBuffer.length;
 			let newSegment = AudioUtils.CreateBuffer(channels, length, sampleRate);
+
+			let offset = (this.TrimTo(start, 3) * sampleRate) >> 0;
+			let duration = (this.TrimTo(end - start, 3) * sampleRate) >> 0;
 			
 			for (let i=0; i<channels; ++i) {
+				let chanData = originalBuffer.getChannelData(i);
 				let fxChanData = renderedBuffer.getChannelData(i);
 				let uberChanData = newSegment.getChannelData(i);
 
-				uberChanData.set(fxChanData);
+				uberChanData.set(chanData.slice(0, offset));
+				uberChanData.set(fxChanData, offset);
+				// uberChanData.set(chanData.slice(offset + duration, length), offset);
 			}
 
 			AudioUtils.LoadDecoded(data, newSegment);
-
 		};
 	}
 };
